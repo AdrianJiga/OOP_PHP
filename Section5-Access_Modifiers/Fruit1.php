6ee29609e3130f912465dd54853e04b3ee61e707
Adrian Jiga <adrian.jiga@RealPage.com>
2022-02-05 17:48:35 +0200

    update Fruit1 properties access modifiers, add getters to access the private properties

--- Section5-Access_Modifiers/Fruit1.php
+++ Section5-Access_Modifiers/Fruit1.php
@@ -2,28 +2,40 @@
 
 class Fruit1 {
 
-    public $name;
-    public $color;
-    public $weight;
+    private $name;
+    private $color;
+    private $weight;
 
-    //public - the property or method can be accessed from everywhere. This is default
-    function set_name($n) {  // a public function (default)
-        $this->name = $n;
+    function set_name($name) {
+        $this->name = $name;
     }
 
-    //protected - the property or method can be accessed within the class and by classes derived from that class
-    protected function set_color($n) { // a protected function
-        $this->color = $n;
+    function set_color($color) {
+        $this->color = $color;
     }
 
-    //private - the property or method can ONLY be accessed within the class
-    private function set_weight($n) { // a private function
-        $this->weight = $n;
+    function set_weight($weight) {
+        $this->weight = $weight;
+    }
+
+    function getColor() {
+        return $this->color;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getWeight() {
+        return $this->weight;
     }
 }
 
 $mango = new Fruit1();
-$mango->set_name('Mango'); // OK
-$mango->set_color('Yellow'); // ERROR
-$mango->set_weight('300'); // ERROR
+
+$mango->set_name('Mango');
+$mango->set_color('Yellow');
+$mango->set_weight('300');
+
+echo "The fruit is " . $mango->getName() . ". It has a " . $mango->getColor() . " color and its weight is " . $mango->getWeight();
 
