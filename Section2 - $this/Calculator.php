5449889bb4ba1965e55eb1bf4eb321dd911a8920
Adrian Jiga <adrian.jiga@RealPage.com>
2022-02-05 15:07:30 +0200

    improve the Calculator.php class with a constructor. END of Constructor and Destructor section

--- Section2 - $this/Calculator.php
+++ Section2 - $this/Calculator.php
@@ -6,6 +6,12 @@ class Calculator{
     public $b;
     public $result;
 
+    function __construct($a, $b){
+        $this->a = $a;
+        $this->b = $b;
+
+    }
+
     public function getA(){
         return $this->a;
     }
@@ -16,18 +22,9 @@ class Calculator{
 
     public function getResult(){
         return $this->result = $this->getA() + $this->getB();
-    }
 
-    public function setA($a): void{
-        $this->a = $a;
-    }
-
-    public function setB($b): void{
-        $this->b = $b;
     }
 }
 
-$calculator = new Calculator();
-$calculator->setA(4);
-$calculator->setB(100);
+$calculator = new Calculator(4, 100);
 echo $calculator->getResult();
