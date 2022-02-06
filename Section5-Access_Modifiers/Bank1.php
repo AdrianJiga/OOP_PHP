7a10ea1d2ce8d160ed09538d2dc7a0496940ab9b
Adrian Jiga <adrian.jiga@RealPage.com>
2022-02-06 14:18:07 +0200

    add copy of Bank.php to section 5 folder, then update it to use access modifiers, getters and setters

---
+++ Section5-Access_Modifiers/Bank1.php
@@ -0,0 +1,36 @@
+<?php
+
+class Bank1{
+
+    private $balance;
+
+    function __construct($balance) {
+        $this->balance = $balance;
+    }
+
+    public function getBalance() {
+        return $this->balance;
+    }
+
+    public function setBalance($balance): void {
+        $this->balance = $balance;
+    }
+
+    function depositAmount($amount){
+        $this->balance = $this->balance + $amount;
+    }
+
+    function deductAmount($amount){
+        if ($this->balance <= 0) {
+            echo "No money in the account" . "<br>";
+        } elseif ($this->balance <= $amount) {
+            echo "The amount requested to be deducted is greater than the balance." . "<br>";
+        }
+        $this->balance = $this->balance - $amount;
+    }
+}
+
+$card = new Bank1(400);
+$card->depositAmount(342);
+$card->deductAmount(287);
+echo "The balance is equal to " . $card->getBalance();
